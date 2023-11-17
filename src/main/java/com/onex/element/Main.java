4c7a1f479dd0bacf39c9cab223acc9b73412ac32
Oleg Primolennyy <Oleg.Primolennyy@x5.ru>
2023-11-18 00:47:24 +0700

    5. add queries

--- src/main/java/com/onex/element/Main.java
+++ src/main/java/com/onex/element/Main.java
@@ -1,8 +1,13 @@
 package com.onex.element;
 
+import java.util.List;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 
 public class Main {
   public static void main(String[] args) {
@@ -13,6 +18,33 @@ public class Main {
 
     Account account = session.get(Account.class, 1);
 
-    System.out.println("Найденный аккаунт: " + account);
+    System.out.println("Найденный аккаунт с ID 1: " + account);
+
+    Account newAccount = new Account();
+    newAccount.setBalance(1000);
+    newAccount.setFullName("Jack Green");
+
+    session.beginTransaction();
+    session.save(newAccount);
+    session.getTransaction().commit();
+
+    // Запрос с Criteria
+    CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
+    CriteriaQuery<Account> criteriaQuery = criteriaBuilder.createQuery(Account.class);
+    Root<Account> root = criteriaQuery.from(Account.class);
+    criteriaQuery.select(root).where(criteriaBuilder.equal(root.get("balance"), 600));
+    List<Account> accounts = session.createQuery(criteriaQuery).getResultList();
+    for (Account acc : accounts) {
+      System.out.println("Найденный аккаунт c балансом 600: " + account);
+    }
+
+    // Запрос с HQL
+    Query<Account> query =
+        session.createQuery("FROM Account WHERE balance = :balanceValue", Account.class);
+    query.setParameter("balanceValue", 100);
+    accounts = query.getResultList();
+    for (Account acc : accounts) {
+      System.out.println("Найденный аккаунт с балансом 100: " + account);
+    }
   }
 }
